3f4f2717418a0ca3f3b77f6cacd811d98ad104d4
Nishant Wailkar <nishantw631@gmail.com>
2024-06-22 20:56:37 +0530

    create the basic ui in tkinter , along with fetching data from csv using pandas

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("data/french_words.csv")
+letter_from_data = data.to_dict(orient="records")
+
+def next_card():
+    current_card =  random.choice(letter_from_data)
+    canvas.itemconfig(card_title , text = "French")
+    canvas.itemconfig(card_word , text = current_card["French"])
 
 
 window = Tk()
@@ -11,20 +21,20 @@ window.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800 , height=526)
 card_front_image = PhotoImage(file="images/card_front.png")
 canvas.create_image(400,263,image=card_front_image)
-canvas.create_text(400,150,text="Title" , font=("Arial" , 40 , "italic"))
-canvas.create_text(400,270,text="Word" , font=("Arial" , 50 , "bold"))
+card_title = canvas.create_text(400,150,text="" , font=("Arial" , 40 , "italic"))
+card_word =  canvas.create_text(400,270,text="" , font=("Arial" , 50 , "bold"))
 canvas.config(bg=BACKGROUND_COLOR , highlightthickness=0)
 canvas.grid(row=0,column=0,columnspan=2)
 
 cross_button_image = PhotoImage(file="images/wrong.png")
-cross_button = Button(image=cross_button_image,bg=BACKGROUND_COLOR , highlightthickness=0)
+cross_button = Button(image=cross_button_image,bg=BACKGROUND_COLOR , highlightthickness=0 , command=next_card)
 cross_button.grid(row=1,column=0)
 
 check_button_image = PhotoImage(file="images/right.png")
-check_button = Button(image=check_button_image,bg=BACKGROUND_COLOR , highlightthickness=0)
+check_button = Button(image=check_button_image,bg=BACKGROUND_COLOR , highlightthickness=0 , command=next_card)
 check_button.grid(row=1,column=1)
 
-
+next_card()
 
 
 window.mainloop()
